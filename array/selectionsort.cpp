454154963917e600c7b78623922e1a4e1bfbfd42
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2023-08-08 08:05:03 +0530

    selection sort

---
+++ array/selectionsort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+void selectionsort(int arr[], int n)
+{
+    int min, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the lenght of array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    selectionsort(arr, n);
+
+    return 0;
+}
