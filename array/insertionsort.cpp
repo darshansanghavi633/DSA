13006a6df31e8cb2e0f68135815b69789f4ede42
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2023-08-09 20:13:07 +0530

    insertion sort

---
+++ array/insertionsort.cpp
@@ -0,0 +1,35 @@
+// insertion sort
+#include <iostream>
+using namespace std;
+void insertionsort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j = i - 1;
+        while (arr[j] > current && j >= 0)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the lenght of array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    insertionsort(arr, n);
+    return 0;
+}
