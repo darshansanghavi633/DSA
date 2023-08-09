91547253f46273f952d39d7904e688413aca48ec
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2023-08-09 11:04:38 +0530

    bubble sort

---
+++ array/bubblesort.cpp
@@ -0,0 +1,38 @@
+// bubble sort
+#include <iostream>
+using namespace std;
+void bubblesort(int arr[], int n)
+{
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the lenght of array" << endl;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubblesort(arr, n);
+    return 0;
+}
