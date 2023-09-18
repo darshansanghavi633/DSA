40bce6116c082557ab2c9cde4aa3f331230b2713
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2023-09-18 18:43:26 +0530

    Tower of hanoi problem using recurssion

---
+++ recursion/tower-of-hanoi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void TowerOfHanoi(int n, char src, char dest, char helper)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    TowerOfHanoi(n - 1, src, helper, dest);
+    cout << "Move ring from tower " << src << " to " << dest << endl;
+    TowerOfHanoi(n - 1, helper, dest, src);
+}
+int main()
+{
+    TowerOfHanoi(3, 'a', 'c', 'b');
+    return 0;
+}
